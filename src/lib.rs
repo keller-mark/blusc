87ced89084c25608d90655b7da7e3e90108c5f63
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2025-12-04 12:24:53 -0500

    Comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,20 @@
 
-// TODO: implement the compression and decompression functions here.
+/*
+TODO: implement the following compression and decompression functions here.
+- blosc1_cbuffer_metainfo
+- blosc1_cbuffer_validate
+- blosc1_cbuffer_sizes
+- blosc1_getitem
+- blosc2_get_complib_info
+- blosc2_compress
+- blosc2_decompress
+- BLOSC_NOSHUFFLE
+- BLOSC2_MAX_OVERHEAD
+- BLOSC2_CPARAMS_DEFAULTS
+- BLOSC2_DPARAMS_DEFAULTS
+- blosc2_create_cctx
+- blosc2_compress_ctx
+- blosc2_cbuffer_sizes
+- blosc2_create_dctx
+- blosc2_decompress_ctx
+*/
