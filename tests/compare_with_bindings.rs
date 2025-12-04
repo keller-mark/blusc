b4dad6ca5ad83b6e4440a97ebe39139e27fab21d
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2025-12-04 15:10:01 -0500

    More tests

--- tests/compare_with_bindings.rs
+++ tests/compare_with_bindings.rs
@@ -20,7 +20,6 @@ use blusc::api::{
     blosc2_compress_ctx as blusc_blosc2_compress_ctx,
     blosc2_create_dctx as blusc_blosc2_create_dctx,
     blosc2_decompress_ctx as blusc_blosc2_decompress_ctx,
-    blosc2_cbuffer_sizes as blusc_blosc2_cbuffer_sizes,
     BLOSC2_CPARAMS_DEFAULTS as BLUSC_BLOSC2_CPARAMS_DEFAULTS,
     BLOSC2_DPARAMS_DEFAULTS as BLUSC_BLOSC2_DPARAMS_DEFAULTS,
 };
@@ -63,7 +62,38 @@ fn roundtrip_blosc_compress_then_blusc_decompress() {
 
 #[test]
 fn roundtrip_blusc_compress_then_blosc_decompress() {
- // TODO
+    unsafe {
+        bound_blosc2_init();
+
+        let text =
+            "I am here writing some very cool and novel words which I will compress and decompress";
+
+        let bytes = text.as_bytes();
+
+        let mut compressed = vec![0; bytes.len() * 2 + BOUND_BLOSC2_MAX_OVERHEAD as usize];
+
+        let stat = blusc_blosc2_compress(
+            5,
+            BOUND_BLOSC_NOSHUFFLE as _,
+            std::mem::size_of::<u8>(),
+            bytes,
+            &mut compressed,
+        );
+        assert!(stat > 0);
+
+        let mut outtext = vec![0_u8; bytes.len()];
+        let stat = bound_blosc2_decompress(
+            compressed.as_ptr().cast(),
+            stat,
+            outtext.as_mut_ptr().cast(),
+            outtext.len() as i32,
+        );
+        assert!(stat > 0);
+
+        assert_eq!(text, std::str::from_utf8(&outtext).unwrap());
+
+        bound_blosc2_destroy();
+    }
 }
 
 #[test]
@@ -141,5 +171,75 @@ fn floats_roundtrip_blosc_compress_then_blusc_decompress() {
 
 #[test]
 fn floats_roundtrip_blusc_compress_then_blosc_decompress() {
-    // TODO
+    // generate numerical data
+    let src: Vec<f32> = (0..10000)
+        .map(|num| ((num * 8923) % 100) as f32 / 2f32) // multiply by big prime number
+        .collect();
+
+    // compress
+    let dest: Vec<u8> = {
+        let typesize = std::mem::size_of::<f32>();
+        let src_size = src.len() * typesize;
+        let dest_size = src_size + BOUND_BLOSC2_MAX_OVERHEAD as usize;
+        let mut dest = vec![0; dest_size];
+
+        let rsize = {
+            let mut cparams = BLUSC_BLOSC2_CPARAMS_DEFAULTS;
+            cparams.clevel = 5;
+            cparams.typesize = typesize as i32;
+            let context = blusc_blosc2_create_cctx(cparams);
+            
+            let src_bytes = unsafe {
+                std::slice::from_raw_parts(
+                    src.as_ptr() as *const u8,
+                    src_size
+                )
+            };
+
+            blusc_blosc2_compress_ctx(
+                &context,
+                src_bytes,
+                &mut dest,
+            )
+        };
+
+        assert!(rsize > 0);
+        dest.into_iter().take(rsize as usize).collect()
+    };
+
+    // decompress
+    let result = {
+        let mut nbytes: i32 = 0;
+        let mut _cbytes: i32 = 0;
+        let mut _blocksize: i32 = 0;
+        unsafe {
+            bound_blosc2_cbuffer_sizes(
+                dest.as_ptr().cast(),
+                &mut nbytes,
+                &mut _cbytes,
+                &mut _blocksize,
+            )
+        };
+        assert!(nbytes != 0);
+        let dest_size = nbytes / std::mem::size_of::<f32>() as i32;
+        let mut result = vec![0f32; dest_size as usize];
+        
+        let error = unsafe {
+            let dparams = BOUND_BLOSC2_DPARAMS_DEFAULTS;
+            let context = bound_blosc2_create_dctx(dparams);
+            
+            bound_blosc2_decompress_ctx(
+                context,
+                dest.as_ptr().cast(),
+                dest.len() as i32,
+                result.as_mut_ptr().cast(),
+                result.len() as i32 * std::mem::size_of::<f32>() as i32,
+            )
+        };
+        assert!(error >= 1);
+        result
+    };
+
+    // check if the values in both arrays are equal
+    assert_eq!(src, result);
 }
