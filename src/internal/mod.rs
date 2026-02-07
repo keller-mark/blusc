0bd80c2cf48fb6eaa351e12e12034a9b14a5a307
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2026-02-07 18:12:01 -0500

    Fix bug

--- src/internal/mod.rs
+++ src/internal/mod.rs
@@ -201,11 +201,8 @@ fn compute_blocksize(
         blocksize = nbytes;
     }
 
-    // TODO: fails with a division by zero error when typesize=0.
-    // This is occurring in zarrs for the tests "codec_blosc_round_trip_snappy" and "codec_blosc_round_trip3",
-    // neither of which specify the "typesize" param
-    // (but both specify "shuffle": "noshuffle" and "blocksize": 0).
-    if blocksize > typesize {
+    // blocksize must be a multiple of typesize (matching C's stune.c)
+    if typesize > 0 && blocksize > typesize {
         blocksize = (blocksize / typesize) * typesize;
     }
 
